aa12b9fbcf464c71d27ad5eb0ce289a486a83706
Олеся <feokrasnodar@yandex.ru>
2023-07-02 21:38:46 +0300

    Добавила домашку по заданиям 1 и 2

---
+++ domashka/Farmer.java
@@ -0,0 +1,15 @@
+public class Farmer extends Character {
+
+  public Farmer(String name, int experience, int level, int hp, int damage, int defense, int speed) {
+    super(name, experience, level, hp, damage, defense, speed);
+  }
+  public Farmer(String name) {
+    super(name);
+  }
+    @Override
+    public String getInfo() {
+      return "Farmer";
+    }
+    @Override
+    public void step(){}
+  }
